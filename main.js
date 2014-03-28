037b6ca25f034137b3f1512392937c3a31035bc1
root <root@psrafo.psrafo>
2014-03-28 08:51:20 -0400

    asdf

--- main.js
+++ main.js
@@ -1,6 +1,138 @@
 var obj = new Object;
 	obj.list = new Object;
 
+function init()
+{
+	if(localStorage.getItem("projects") == null)
+		normalizeObj();
+		
+	obj = JSON.parse(localStorage.getItem("projects"));
+	
+	normalizeObj();
+	updateList();
+}
+
+function addToList(title, text)
+{
+	size = Object.keys(obj.list).length;
+	obj.list[size]={
+		"title": title,
+		"text": text,
+		"num": size
+	};
+	
+	normalizeObj();
+	updateList();
+}
+
+function removeFromList(item_id)
+{
+	delete obj.list[item_id];
+	normalizeObj();
+	
+	$(".item__tpl1[rel=" + item_id + "]").slideUp(500);
+	
+	resetEditForm();
+	setTimeout(updateList, 500);
+}
+
+function resetEditForm()
+{
+	document.getElementById("edit_area").setAttribute("rel", "");
+	document.getElementById("edit_area__title").value = "";
+	document.getElementById("edit_area__text").value = "";
+	$("#edit_area").slideUp(150);
+}
+
+function updateList()
+{
+	document.getElementById("main_list").innerHTML = "";
+	arr = obj.list;
+	size = Object.keys(obj.list).length;
+	for(n in arr)
+	{
+		for(l = 0; l < size; l++)
+		{
+			if(l == arr[n].num)
+			{
+				classes = "";
+				if(arr[n].isRed != null)
+					classes = "red";
+				
+				str = ""+
+					"<div class='item__tpl1 " + classes + "' rel='" + n + "' num='" + arr[n].num + "'>" +
+						"<div class='item__tpl1__left' onclick='getSolving(this);'>" +
+							"<div class='item__tpl1__left__title'>" + arr[n].title + "</div>" +
+						"</div>" +
+						"<div class='item__tpl1__right'>" +
+							"<div class='btn__tpl1' onclick='prepareToEdit(" + n + ")'>Edit</div>" +
+						"</div>" +
+						"<div class='clearfix'></div>" +
+					"</div>" +
+				"";
+				
+				document.getElementById("main_list").innerHTML = document.getElementById("main_list").innerHTML + str
+			}
+		}
+	}
+	
+	$("#main_list .item__tpl1").sort(function (a, b) {
+		return +a.getAttribute("num") - +b.getAttribute("num");
+	}).appendTo("#main_list");	
+}
+
+function prepareToEdit(item_id)
+{
+	$("#edit_area").slideDown(150);
+	document.getElementById("edit_area").setAttribute("rel", item_id);
+	document.getElementById("edit_area__title").value = obj.list[item_id].title;
+	document.getElementById("edit_area__text").value = obj.list[item_id].text;
+}
+
+function editItem(item_id, title, text)
+{
+	obj.list[item_id].title = title;
+	obj.list[item_id].text = text;
+	
+	normalizeObj();
+	updateList();
+	resetEditForm();
+}
+
+function getSolving(elem)
+{
+	score = elem.parentNode;
+	item_id = parseInt(score.getAttribute("rel"));
+	
+	if(score.classList.contains("red"))
+	{
+		delete obj.list[item_id].isRed;
+		score.classList.remove("red");
+	}
+	else
+	{
+		obj.list[item_id].isRed = true;
+		score.classList.add("red");
+	}
+	
+	normalizeObj();
+}
+
+function normalizeObj()
+{
+	localStorage.setItem("projects", JSON.stringify(obj));
+}
+
+function destroyList()
+{
+	localStorage.removeItem("projects");
+	
+	obj = new Object;
+	obj.list = new Object;
+	normalizeObj();
+	updateList();
+}
+
 $(document).ready(function() {
 	init();
 	
@@ -163,135 +295,3 @@ $(document).ready(function() {
 	});
 	
 });
-
-function init()
-{
-	if(localStorage.getItem("projects") == null)
-		normalizeObj();
-		
-	obj = JSON.parse(localStorage.getItem("projects"));
-	
-	normalizeObj();
-	updateList();
-}
-
-function addToList(title, text)
-{
-	size = Object.keys(obj.list).length;
-	obj.list[size]={
-		"title": title,
-		"text": text,
-		"num": size
-	};
-	
-	normalizeObj();
-	updateList();
-}
-
-function removeFromList(item_id)
-{
-	delete obj.list[item_id];
-	normalizeObj();
-	
-	$(".item__tpl1[rel=" + item_id + "]").slideUp(500);
-	
-	resetEditForm();
-	setTimeout(updateList, 500);
-}
-
-function resetEditForm()
-{
-	document.getElementById("edit_area").setAttribute("rel", "");
-	document.getElementById("edit_area__title").value = "";
-	document.getElementById("edit_area__text").value = "";
-	$("#edit_area").slideUp(150);
-}
-
-function updateList()
-{
-	document.getElementById("main_list").innerHTML = "";
-	arr = obj.list;
-	size = Object.keys(obj.list).length;
-	for(n in arr)
-	{
-		for(l = 0; l < size; l++)
-		{
-			if(l == arr[n].num)
-			{
-				classes = "";
-				if(arr[n].isRed != null)
-					classes = "red";
-				
-				str = ""+
-					"<div class='item__tpl1 " + classes + "' rel='" + n + "' num='" + arr[n].num + "'>" +
-						"<div class='item__tpl1__left' onclick='getSolving(this);'>" +
-							"<div class='item__tpl1__left__title'>" + arr[n].title + "</div>" +
-						"</div>" +
-						"<div class='item__tpl1__right'>" +
-							"<div class='btn__tpl1' onclick='prepareToEdit(" + n + ")'>Edit</div>" +
-						"</div>" +
-						"<div class='clearfix'></div>" +
-					"</div>" +
-				"";
-				
-				document.getElementById("main_list").innerHTML = document.getElementById("main_list").innerHTML + str
-			}
-		}
-	}
-	
-	$("#main_list .item__tpl1").sort(function (a, b) {
-		return +a.getAttribute("num") - +b.getAttribute("num");
-	}).appendTo("#main_list");	
-}
-
-function prepareToEdit(item_id)
-{
-	$("#edit_area").slideDown(150);
-	document.getElementById("edit_area").setAttribute("rel", item_id);
-	document.getElementById("edit_area__title").value = obj.list[item_id].title;
-	document.getElementById("edit_area__text").value = obj.list[item_id].text;
-}
-
-function editItem(item_id, title, text)
-{
-	obj.list[item_id].title = title;
-	obj.list[item_id].text = text;
-	
-	normalizeObj();
-	updateList();
-	resetEditForm();
-}
-
-function getSolving(elem)
-{
-	score = elem.parentNode;
-	item_id = parseInt(score.getAttribute("rel"));
-	
-	if(score.classList.contains("red"))
-	{
-		delete obj.list[item_id].isRed;
-		score.classList.remove("red");
-	}
-	else
-	{
-		obj.list[item_id].isRed = true;
-		score.classList.add("red");
-	}
-	
-	normalizeObj();
-}
-
-function normalizeObj()
-{
-	localStorage.setItem("projects", JSON.stringify(obj));
-}
-
-function destroyList()
-{
-	localStorage.removeItem("projects");
-	
-	obj = new Object;
-	obj.list = new Object;
-	normalizeObj();
-	updateList();
-}
